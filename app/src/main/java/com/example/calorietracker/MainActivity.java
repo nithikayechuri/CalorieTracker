7d69a174daf9548162f211cf31e02089c53f961f
nithikayechuri <101083763+nithikayechuri@users.noreply.github.com>
2022-03-13 19:46:36 -0700

    added edit text

--- app/src/main/java/com/example/calorietracker/MainActivity.java
+++ app/src/main/java/com/example/calorietracker/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calorietracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText ageText= findViewById(R.id.editTextTextPersonName2);
+        Button calculateText= findViewById(R.id.button2);
+        EditText weightText= findViewById(R.id.editTextTextPersonName4);
+        EditText heightText= findViewById(R.id.editTextTextPersonName5);
+        EditText inchesText= findViewById(R.id.editTextTextPersonName6);
+        calculateText.setOnClickListener(buttonClickListener);
     }
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v){
+
+            }
+        };
+
 }
